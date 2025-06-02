541bed9fdbce1515d3255ac778ddeda7439b444d
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-02 11:55:49 +0330

    add maui app to project

---
+++ TikTime.MauiApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TikTime.MauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TikTime.MauiApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TikTime.MauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
