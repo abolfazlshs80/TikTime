7389cdaf4dd572323ee93754e118b2ea06cdbb8f
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-03 15:34:32 +0330

    add view page number

---
+++ TikTime.MauiApp/MVVM/View/Account/LoginNumberPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace TikTime.MauiApp.MVVM.View.Account;
+
+public partial class LoginNumberPage : ContentPage
+{
+	public LoginNumberPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void LoginButton_OnClicked(object? sender, EventArgs e)
+    {
+        if (!NumberTextBox.Text.StartsWith("09") || NumberTextBox.Text.Length.Equals(11))
+        {
+            await DisplayAlert("شماره اشتباه است", "شماره باید با 09 شروع شود و کامل باشد", "باشه");
+            return;
+        }
+            
+    }
+}
