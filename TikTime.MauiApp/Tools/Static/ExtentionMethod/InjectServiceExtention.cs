0f88d8b90991140402a55faba4f3b1935dba599c
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-05 16:11:08 +0330

    Add InjectServiceExtention class to configure service registrations

---
+++ TikTime.MauiApp/Tools/Static/ExtentionMethod/InjectServiceExtention.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TikTime.MauiApp.MVVM.View.Account;
+using TikTime.MauiApp.MVVM.View.Main;
+using TikTime.MauiApp.Service;
+
+namespace TikTime.MauiApp.Tools.Static.ExtentionMethod
+{
+   public static class InjectServiceExtention
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services)
+        {
+            
+            // Register your services here
+            services.AddSingleton<FakeDataService>();
+            services.AddScoped<IUserService, UserService>();
+
+            services.AddTransient<LoginNumberPage>();
+            services.AddTransient<LoginPasswordPage>();
+            services.AddTransient<MainPage>();
+            // Add other services as needed
+            return services;
+        }
+    }
+}
