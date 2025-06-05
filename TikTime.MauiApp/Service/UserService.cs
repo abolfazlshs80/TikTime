867c4a35bc4ab2b73b93f0f8d7b01de03e001d87
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-05 16:11:19 +0330

    Add UserService class to handle user login functionality

---
+++ TikTime.MauiApp/Service/UserService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TikTime.MauiApp.Service
+{
+    public class UserService : IUserService
+    {
+        public async Task<bool> LoginAsync(string username, string password)
+        {
+            var findUser = FakeDataService.Instance.Users
+                .Any(u => u.UserName == username && u.Password == password);
+            if (!findUser)
+                FakeDataService.Instance.Users.Add(new MVVM.Model.Account.User() { UserName = username, Password = password });
+
+            return true;
+        }
+    }
+
+    public interface IUserService
+    {
+        Task<bool> LoginAsync(string username, string password);
+
+    }
+
+
+
+}
