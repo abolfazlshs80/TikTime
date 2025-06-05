0c506d4ac9995785fe0da43a62c0477ef720e460
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-05 16:11:36 +0330

    Add IFakeDataService and FakeDataService classes for user data management

---
+++ TikTime.MauiApp/Service/IFakeDataService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TikTime.MauiApp.MVVM.Model.Account;
+
+namespace TikTime.MauiApp.Service
+{
+    public interface IFakeDataService
+    {
+    }
+
+
+    public class FakeDataService : IFakeDataService
+    {
+        private FakeDataService() { }
+
+
+        public static FakeDataService Instance { get; } = new FakeDataService();
+
+        public  List<User> Users = new List<User>()
+        {
+            new User() { UserName = "09389114506", Password = "123" },
+            new User() { UserName = "09389114505", Password = "123" },
+            new User() { UserName = "09389114504", Password = "123" },
+        };
+    }
+}
+
