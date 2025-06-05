282625ba10282f5e0aff42fb1c72b3896620e240
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-05 16:13:12 +0330

    Add MainPage with layout and initial structure

---
+++ TikTime.MauiApp/MVVM/View/Main/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace TikTime.MauiApp.MVVM.View.Main
+{
+    public partial class MainPage : ContentPage
+    {
+     
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
