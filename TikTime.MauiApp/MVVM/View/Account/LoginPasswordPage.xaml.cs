8560e1032da824f0bf9cb1b0bf80c38bd7c88be8
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-05 16:12:47 +0330

    Implement login functionality with button click event and user authentication

--- TikTime.MauiApp/MVVM/View/Account/LoginPasswordPage.xaml.cs
+++ TikTime.MauiApp/MVVM/View/Account/LoginPasswordPage.xaml.cs
@@ -1,11 +1,17 @@
-﻿namespace TikTime.MauiApp.MVVM.View.Account;
+﻿using TikTime.MauiApp.Service;
 
+namespace TikTime.MauiApp.MVVM.View.Account;
+[QueryProperty(nameof(PhoneNumber), "phone")]
 public partial class LoginPasswordPage : ContentPage
 {
-	public LoginPasswordPage()
-	{
-		InitializeComponent();
-	}
+    public string PhoneNumber { get; set; }
+    private readonly IUserService _userService;
+    public LoginPasswordPage(IUserService userService)
+    {
+        InitializeComponent();
+        this._userService = userService;
+
+    }
 
     private void OnDigitChanged(object sender, TextChangedEventArgs e)
     {
@@ -42,4 +48,21 @@ public partial class LoginPasswordPage : ContentPage
             currentEntry.Unfocus(); // آخرین ورودی
     }
 
+    private async void ButtonLogin_OnClicked(object? sender, EventArgs e)
+    {
+        if (string.IsNullOrEmpty(Digit1.Text) | string.IsNullOrEmpty(Digit2.Text) |
+           string.IsNullOrEmpty(Digit3.Text) | string.IsNullOrEmpty(Digit4.Text) |
+           string.IsNullOrEmpty(Digit5.Text) | string.IsNullOrEmpty(Digit6.Text))
+            return;
+        var IsAuthenticated = await _userService.LoginAsync(PhoneNumber, Digit1.Text + Digit2.Text + Digit3.Text + Digit4.Text + Digit5.Text + Digit6.Text);
+        if (IsAuthenticated)
+            await Shell.Current.GoToAsync($"//MainPage");
+
+        else
+            await DisplayAlert("خطا", "رمز عبور اشتباه است", "باشه");
+
+
+
+
+    }
 }
