ed6f8c5eacfae6f0794fa771fcc7ea6d3145321c
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-06 12:40:30 +0330

    Add customer-related services and pages to service configuration

--- TikTime.MauiApp/Tools/Static/ExtentionMethod/InjectServiceExtention.cs
+++ TikTime.MauiApp/Tools/Static/ExtentionMethod/InjectServiceExtention.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TikTime.MauiApp.MVVM.View.Account;
+using TikTime.MauiApp.MVVM.View.Customer;
 using TikTime.MauiApp.MVVM.View.Main;
 using TikTime.MauiApp.Service;
 
@@ -17,10 +18,22 @@ namespace TikTime.MauiApp.Tools.Static.ExtentionMethod
             // Register your services here
             services.AddSingleton<FakeDataService>();
             services.AddScoped<IUserService, UserService>();
+            services.AddScoped<ICustomerService, CustomerService>();
 
+           
+            return services;
+        }
+        public static IServiceCollection ConfigurePage(this IServiceCollection services)
+        {
+
+      
             services.AddTransient<LoginNumberPage>();
             services.AddTransient<LoginPasswordPage>();
             services.AddTransient<MainPage>();
+            services.AddTransient<AddCustomerDialog>();
+            services.AddTransient<AddCustomerPage>();
+            services.AddTransient<AddPage>();
+            services.AddTransient<SettingPage>();
             // Add other services as needed
             return services;
         }
