42b2ecafb5b4a528f48e72908e010e6366aac2cc
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-06 12:41:56 +0330

    Add Customer model with properties for Id, Name, and Phone

---
+++ TikTime.MauiApp/MVVM/Model/Customer/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TikTime.MauiApp.MVVM.Model.Customer
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
