48b80d33cda884f41dfbf42416b3ccca6942a14e
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-06 12:41:39 +0330

    Add AddCustomerPage with UI and event handlers for note, excel file, and group contacts actions

---
+++ TikTime.MauiApp/MVVM/View/Customer/AddCustomerPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Mopups.Services;
+
+namespace TikTime.MauiApp.MVVM.View.Customer;
+
+public partial class AddCustomerPage : ContentPage
+{
+    private IServiceProvider _serviceProvider;
+
+    public AddCustomerPage(IServiceProvider serviceProvider)
+	{
+		InitializeComponent();
+        _serviceProvider = serviceProvider;
+    }
+
+
+    private async void OnAddNoteClicked(object sender, EventArgs e)
+    {
+        // Handle add note action
+        // await DisplayAlert("افزودن نکی", "Add note functionality", "OK");
+
+
+        await MopupService.Instance.PushAsync(new AddCustomerDialog(_serviceProvider));
+    }
+
+    private async void OnExcelFileClicked(object sender, EventArgs e)
+    {
+        // Handle excel file action
+        await DisplayAlert("فایل اکسل", "Excel file functionality", "OK");
+    }
+
+    private async void OnGroupContactsClicked(object sender, EventArgs e)
+    {
+        // Handle group contacts action
+        await DisplayAlert("مخاطبین گروهی", "Group contacts functionality", "OK");
+    }
+}
