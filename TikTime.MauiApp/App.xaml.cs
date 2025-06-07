80791f13d18ad7dc7f655c23e6c5847b83915a23
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-07 16:25:06 +0330

    Register routes for AddCustomerPage, EditCustomerPage, and ListCustomerPage in the application

--- TikTime.MauiApp/App.xaml.cs
+++ TikTime.MauiApp/App.xaml.cs
@@ -1,10 +1,15 @@
-﻿namespace TikTime.MauiApp
+﻿using TikTime.MauiApp.MVVM.View.Customer;
+
+namespace TikTime.MauiApp
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(AddCustomerPage), typeof(AddCustomerPage));
+            Routing.RegisterRoute(nameof(EditCustomerPage), typeof(EditCustomerPage));
+            Routing.RegisterRoute(nameof(ListCustomerPage), typeof(ListCustomerPage));
 
             MainPage = new AppShell();
         }
