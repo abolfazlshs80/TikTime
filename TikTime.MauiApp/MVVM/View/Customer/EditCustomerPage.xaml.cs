a05aa98cf591979595ae184f02bfe62bb759a193
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-08 17:23:41 +0330

    Enhance EditCustomerPage with data binding for customer details and selection handling for job, category, and social media platforms

--- TikTime.MauiApp/MVVM/View/Customer/EditCustomerPage.xaml.cs
+++ TikTime.MauiApp/MVVM/View/Customer/EditCustomerPage.xaml.cs
@@ -1,11 +1,49 @@
+using TikTime.MauiApp.MVVM.Model;
+using TikTime.MauiApp.MVVM.Model.Enums;
+using TikTime.MauiApp.MVVM.ViewModel.Customer;
+using TikTime.MauiApp.Service;
+
 namespace TikTime.MauiApp.MVVM.View.Customer;
-[QueryProperty(nameof(CustomerId), "CustomerId")]
+//[QueryProperty(nameof(CustomerId), "CustomerId")]
 public partial class EditCustomerPage : ContentPage
 {
     public int CustomerId { get; set; }
-	public EditCustomerPage()
+	public EditCustomerPage(IServiceProvider serviceProvider)
 	{
 		InitializeComponent();
-	
-	}
+        CustomerId = int.Parse(NavigationDataStore.Instance.Parameter.ToString());
+        var model = new EditCustomerViewModel(serviceProvider){CustomerId = CustomerId};
+        model.CustomerId = CustomerId;
+        model.LoadCustomer();
+        BindingContext = model;
+
+
+    }
+
+    private void JobPicker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (BindingContext is EditCustomerViewModel vm)
+        {
+            var selectedJob = (CustomerJob)JobPicker.SelectedItem;
+            vm.OnJobSelectedCommand?.Execute(selectedJob);
+        }
+    }
+
+    private void CustomerCategoryPicker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (BindingContext is EditCustomerViewModel vm)
+        {
+            var selected = (CustomerCategory)CustomerCategoryPicker.SelectedItem;
+            vm.OnJobSelectedCommand?.Execute(selected);
+        }
+    }
+
+    private void SocialMediaPlatformsPicker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (BindingContext is EditCustomerViewModel vm)
+        {
+            var selected = (SocialMediaPlatforms)SocialMediaPlatformsPicker.SelectedItem;
+            vm.OnJobSelectedCommand?.Execute(selected);
+        }
+    }
 }
