96191043cd059f48a3c497cccef0dd13d2ffde40
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-08 17:23:03 +0330

    Refactor Customer model to include default values for Desc, SocialMediaPlatforms, CustomerJob, CustomerCategory, and BrithDay properties

--- TikTime.MauiApp/MVVM/Model/Customer/Customer.cs
+++ TikTime.MauiApp/MVVM/Model/Customer/Customer.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MockPars.Application.Extention;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TikTime.MauiApp.MVVM.Model.Enums;
 
 namespace TikTime.MauiApp.MVVM.Model.Customer
 {
@@ -11,6 +13,11 @@ namespace TikTime.MauiApp.MVVM.Model.Customer
         public int Id { get; set; }
         public string Name { get; set; }
         public string Phone { get; set; }
+        public string Desc { get; set; } = "dont have desc";
         public DateOnly Date { get; set; }
+        public SocialMediaPlatforms SocialMediaPlatforms { get; set; } = SocialMediaPlatforms.ContactsAndFriends;
+        public CustomerJob CustomerJob { get; set; } = CustomerJob.Other;
+        public CustomerCategory CustomerCategory { get; set; } = CustomerCategory.Employees;
+        public string BrithDay { get; set; }=DateTime.Now.ToShamsi();
     }
 }
