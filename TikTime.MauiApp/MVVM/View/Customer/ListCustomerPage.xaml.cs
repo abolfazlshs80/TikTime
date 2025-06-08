c9bc1f333ef60865a0f28b663bf4894ca1f92a3a
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-08 17:23:46 +0330

    Refactor ListCustomerPage by cleaning up unused code and comments

--- TikTime.MauiApp/MVVM/View/Customer/ListCustomerPage.xaml.cs
+++ TikTime.MauiApp/MVVM/View/Customer/ListCustomerPage.xaml.cs
@@ -1,3 +1,6 @@
+using MauiPersianToolkit.Controls;
+using MauiPersianToolkit.Models;
+using MauiPersianToolkit.Services.Dialog;
 using TikTime.MauiApp.MVVM.ViewModel.Customer;
 using TikTime.MauiApp.Service;
 
@@ -11,10 +14,39 @@ public partial class ListCustomerPage : ContentPage
         InitializeComponent();
         this._customerService = serviceProvider.GetRequiredService<ICustomerService>();
         BindingContext = new ListCustomerViewModel(serviceProvider);
+
+        //DialogService dialogService = new DialogService();
+
+
+
+        //// Custom Dialog
+        //dialogService.CustomDialog(new CustomDialogConfig()
+        //{
+        //    Title = "Register Information",
+        //    AcceptText = "Register",
+        //    CancelText = "Cancle",
+        //    Message = "Enter Your Info",
+        //    Icon = MessageIcon.QUESTION,
+        //    AcceptIcon = MessageIcon.QUESTION,
+        //    Cancelable = true,
+        //    CancelIcon = MessageIcon.ERROR,
+        //    DialogColor = Colors.DeepPink,
+        //    CloseWhenBackgroundIsClicked = false,
+        //    CloseAfterAccept = true,
+        //    OnAction = new Action<bool>((arg) => { }),
+        //    Content = new StackLayout()
+        //    {
+        //        Children =
+        //        {
+        //            new MauiPersianToolkit.Controls.DatePicker(){ PlaceHolder = "BirthDate" }
+        //        }
+        //    }
+        //});
     }
 
     private async void AddCuctomerTapGestureRecognizer_OnTapped(object? sender, TappedEventArgs e)
     {
+
         await Shell.Current.GoToAsync($"AddCustomerPage");
     }
 }
