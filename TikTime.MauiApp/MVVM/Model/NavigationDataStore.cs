e659bf2c58fa7a0887dda3bdd5289935b53baf94
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-08 17:22:58 +0330

    Add NavigationDataStore class for centralized navigation data management

---
+++ TikTime.MauiApp/MVVM/Model/NavigationDataStore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TikTime.MauiApp.MVVM.Model
+{
+    public class NavigationDataStore
+    {
+        private NavigationDataStore()
+        {
+            
+        }
+
+        public static NavigationDataStore Instance { get; } = new NavigationDataStore();
+        public object Parameter { get; set; }
+    }
+
+}
