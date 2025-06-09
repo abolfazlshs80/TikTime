411a839dd394b6f375395851390037a36f1422ae
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-09 17:41:13 +0330

    Add Nobat model class with properties for Id, Desc, StartDate, StartTime, ServiceId, and CustomerId

---
+++ TikTime.MauiApp/MVVM/Model/Nobat/Nobat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TikTime.MauiApp.MVVM.Model.Nobat
+{
+    public class Nobat
+    {
+
+
+        public int Id { get; set; }
+        public string Desc { get; set; }
+        public string StartDate { get; set; }
+        public string StartTime { get; set; }
+
+
+        public int ServiceId { get; set; }
+        public Service.Service Service { get; set; } = new();
+
+
+        public int CustomerId { get; set; }
+        public Customer.Customer Customer { get; set; } = new();
+    }
+}
