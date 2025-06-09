d38b55ba216f5e0a95e6faa131971ee507997597
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-09 17:42:15 +0330

    Add NobatService and INobatService for managing Nobat entities

---
+++ TikTime.MauiApp/Service/NobatService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TikTime.MauiApp.MVVM.Model.Nobat;
+
+namespace TikTime.MauiApp.Service
+{
+    public class NobatService : INobatService
+    {
+        public async Task<bool> Add(Nobat nobat)
+        {
+            FakeDataService.Instance.Nobat.Add(nobat);
+            return true;
+        }
+    }
+
+    public interface INobatService
+    {
+        Task<bool> Add(Nobat nobat);
+
+    }
+
+
+
+}
