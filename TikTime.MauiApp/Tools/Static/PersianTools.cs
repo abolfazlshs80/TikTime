d5dfb33e9f4540687ada9aaaa0dc0f027acf9216
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-10 19:33:40 +0330

    Add PersianTools class with GetPersianMonthName method for retrieving Persian month names

---
+++ TikTime.MauiApp/Tools/Static/PersianTools.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TikTime.MauiApp.Tools.Static
+{
+    public class PersianTools
+    {
+        public static string GetPersianMonthName(int month)
+        {
+            string[] persianMonths =
+            {
+                "فروردین",
+                "اردیبهشت",
+                "خرداد",
+                "تیر",
+                "مرداد",
+                "شهریور",
+                "مهر",
+                "آبان",
+                "آذر",
+                "دی",
+                "بهمن",
+                "اسفند"
+            };
+
+            if (month >= 1 && month <= 12)
+                return persianMonths[month - 1];
+
+            return "ماه نامعتبر";
+        }
+
+    }
+}
