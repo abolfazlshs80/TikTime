b657fcbf4b4c999d5b9b4956bfdeb7560bddefee
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-10 19:33:44 +0330

    Fix namespace declaration and add Shamsi date conversion methods in DateExtension class

--- TikTime.MauiApp/Tools/Static/ExtentionMethod/DateExtension.cs
+++ TikTime.MauiApp/Tools/Static/ExtentionMethod/DateExtension.cs
@@ -1,6 +1,6 @@
 ﻿using System.Globalization;
 
-namespace MockPars.Application.Extention
+namespace TikTime.MauiApp.Tools.Static.ExtentionMethod
 {
     public static class DateExtension
     {
@@ -15,6 +15,22 @@ namespace MockPars.Application.Extention
             var prCalender = new PersianCalendar();
             return prCalender.GetYear(date.Value).ToString("00") + "/"
                 + prCalender.GetMonth(date.Value).ToString("00") + "/" + prCalender.GetDayOfMonth(date.Value).ToString("00");
+        }
+
+        public static int DayToShamsi(this DateTime date)
+        {
+            var prCalender = new PersianCalendar();
+            return prCalender.GetDayOfMonth(date);
+        }
+        public static int MonthToShamsi(this DateTime date)
+        {
+            var prCalender = new PersianCalendar();
+            return prCalender.GetMonth(date);
+        }
+        public static int YearToShamsi(this DateTime date)
+        {
+            var prCalender = new PersianCalendar();
+            return prCalender.GetYear(date);
         }
         public static DateTime ToMiladi(this string persianDate)
         {
