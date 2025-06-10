73c43c6d7dab7a2915b1b42db971a2bea8245517
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-10 19:33:16 +0330

    Add ListNobatPage and its code-behind for managing Nobat appointments

---
+++ TikTime.MauiApp/MVVM/View/Nobats/ListNobatPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Collections.ObjectModel;
+using TikTime.MauiApp.MVVM.Model;
+using TikTime.MauiApp.MVVM.ViewModel.Nobat;
+
+namespace TikTime.MauiApp.MVVM.View.Nobats;
+
+public partial class ListNobatPage : ContentPage
+{
+    public ListNobatPage(IServiceProvider serviceProvider)
+    {
+        InitializeComponent();
+
+        // Initialize sample data
+      
+        var model = new ListNobatViewModel(serviceProvider);
+        model.LoadData(DateTime.Now);
+        BindingContext = model;
+      
+    }
+
+    private async void OnAddAppointmentClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("ثبت نوبت", "Add appointment functionality", "OK");
+    }
+
+    private async void OnTimeSlotClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("انتخاب زمان", "Time slot selection", "OK");
+    }
+
+    private async void AddNobatTapGestureRecognizer_OnTapped(object? sender, TappedEventArgs e)
+    {
+        NavigationDataStore.Instance.Parameter = null;
+        await Shell.Current.GoToAsync($"AddNobatPage");
+    }
+}
+
