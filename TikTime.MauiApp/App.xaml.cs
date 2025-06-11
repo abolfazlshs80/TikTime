cbcd5aaaa857141d00759df19940ce90541651f3
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-11 16:21:01 +0330

    Add routing for MainPage in App.xaml.cs

--- TikTime.MauiApp/App.xaml.cs
+++ TikTime.MauiApp/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using TikTime.MauiApp.MVVM.View.Calender;
 using TikTime.MauiApp.MVVM.View.Customer;
+using TikTime.MauiApp.MVVM.View.Main;
 using TikTime.MauiApp.MVVM.View.Nobats;
 
 namespace TikTime.MauiApp
@@ -15,6 +16,7 @@ namespace TikTime.MauiApp
             Routing.RegisterRoute(nameof(CustomCalenderPage), typeof(CustomCalenderPage));
             Routing.RegisterRoute(nameof(AddNobatPage), typeof(AddNobatPage));
             Routing.RegisterRoute(nameof(ListNobatPage), typeof(ListNobatPage));
+            Routing.RegisterRoute(nameof(MVVM.View.Main.MainPage), typeof(MainPage));
 
             MainPage = new AppShell();
         }
