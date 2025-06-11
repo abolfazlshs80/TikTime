9519d21c9ea65552acc5ccea549cf9b32ab97398
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-11 16:21:36 +0330

    Refactor MainPage.xaml.cs to improve method organization and enhance user interaction feedback

--- TikTime.MauiApp/MVVM/View/Main/MainPage.xaml.cs
+++ TikTime.MauiApp/MVVM/View/Main/MainPage.xaml.cs
@@ -23,6 +23,32 @@
         {
             await Shell.Current.GoToAsync($"ListNobatPage");
         }
+
+
+        private void OnSettingsClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Settings", "Settings clicked", "OK");
+        }
+
+        private void OnListClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("List", "List clicked", "OK");
+        }
+
+        private async void OnAddClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//AddCustomerPage");
+        }
+
+        private void OnSearchClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Search", "Search clicked", "OK");
+        }
+
+        private void OnHomeClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Home", "Home clicked", "OK");
+        }
     }
 
 }
