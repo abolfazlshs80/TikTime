dee134c5d7812f6136ed8ec2fd022adfecdde328
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-15 12:05:40 +0330

    Refactor EditCustomerPage layout and update event handlers for improved functionality

--- TikTime.MauiApp/MVVM/View/Customer/EditCustomerPage.xaml.cs
+++ TikTime.MauiApp/MVVM/View/Customer/EditCustomerPage.xaml.cs
@@ -1,4 +1,4 @@
-using TikTime.MauiApp.MVVM.Model;
+﻿using TikTime.MauiApp.MVVM.Model;
 using TikTime.MauiApp.MVVM.Model.Enums;
 using TikTime.MauiApp.MVVM.ViewModel.Customer;
 using TikTime.MauiApp.Service;
@@ -20,23 +20,72 @@ public partial class EditCustomerPage : ContentPage
 
     }
 
-    private void JobPicker_SelectedIndexChanged(object sender, EventArgs e)
+    private async void OnBackClicked(object sender, EventArgs e)
     {
-        if (BindingContext is EditCustomerViewModel vm)
-        {
-            if (JobPicker.SelectedItem is CustomerJob selectedJob)
-                vm.OnJobSelectedCommand?.Execute(selectedJob);
-        }
+        await Shell.Current.GoToAsync("..");
     }
 
-    private void CustomerCategoryPicker_SelectedIndexChanged(object sender, EventArgs e)
+    private void OnMenuClicked(object sender, EventArgs e)
     {
-        if (BindingContext is EditCustomerViewModel vm)
-        {
-            var selected = (CustomerCategory)CustomerCategoryPicker.SelectedItem;
-            vm.OnJobSelectedCommand?.Execute(selected);
-        }
+        DisplayAlert("Menu", "Menu clicked", "OK");
     }
+
+    private async void OnBirthClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("تولد", "ویرایش تاریخ تولد", "تایید");
+    }
+
+    private async void OnJobClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("شغل", "ویرایش شغل مشتری", "تایید");
+    }
+
+    private async void OnHowFoundUsClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("نحوه آشنایی", "ویرایش نحوه آشنایی", "تایید");
+    }
+
+    private void OnSettingsClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Settings", "Settings clicked", "OK");
+    }
+
+    private void OnListClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("List", "List clicked", "OK");
+    }
+
+    private void OnAddClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Add", "Add clicked", "OK");
+    }
+
+    private void OnSearchClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Search", "Search clicked", "OK");
+    }
+
+    private async void OnHomeClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//MainPage");
+    }
+    //private void JobPicker_SelectedIndexChanged(object sender, EventArgs e)
+    //{
+    //    if (BindingContext is EditCustomerViewModel vm)
+    //    {
+    //        if (JobPicker.SelectedItem is CustomerJob selectedJob)
+    //            vm.OnJobSelectedCommand?.Execute(selectedJob);
+    //    }
+    //}
+
+    //private void CustomerCategoryPicker_SelectedIndexChanged(object sender, EventArgs e)
+    //{
+    //    if (BindingContext is EditCustomerViewModel vm)
+    //    {
+    //        var selected = (CustomerCategory)CustomerCategoryPicker.SelectedItem;
+    //        vm.OnJobSelectedCommand?.Execute(selected);
+    //    }
+    //}
 
     private void SocialMediaPlatformsPicker_SelectedIndexChanged(object sender, EventArgs e)
     {
@@ -47,11 +96,14 @@ public partial class EditCustomerPage : ContentPage
         }
     }
 
-    private void NobatButton_OnClicked(object? sender, EventArgs e)
-    {
-        if (BindingContext is EditCustomerViewModel vm)
-        {
-            vm.OnNobatCommand?.Execute(new object());
-        }
-    }
+    //private void NobatButton_OnClicked(object? sender, EventArgs e)
+    //{
+    //    if (BindingContext is EditCustomerViewModel vm)
+    //    {
+    //        vm.OnNobatCommand?.Execute(new object());
+    //    }
+    //}
 }
+
+
+
