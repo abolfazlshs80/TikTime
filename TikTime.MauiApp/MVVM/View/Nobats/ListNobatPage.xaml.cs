e6912259c167dbdf9ecfd4c7f37b4a7e1a122f56
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-16 17:02:44 +0330

    Refactor ListNobatPage to restore and implement event handlers for menu and navigation actions

--- TikTime.MauiApp/MVVM/View/Nobats/ListNobatPage.xaml.cs
+++ TikTime.MauiApp/MVVM/View/Nobats/ListNobatPage.xaml.cs
@@ -24,5 +24,131 @@ public partial class ListNobatPage : ContentPage
         NavigationDataStore.Instance.Parameter = null;
         await Shell.Current.GoToAsync($"AddNobatPage");
     }
+
+    bool isMenuVisible = false;
+
+    //private async void ToggleMenu(object sender, EventArgs e)
+    //{
+    //    if (isMenuVisible)
+    //    {
+    //        // مخفی کردن منو
+    //        await DropdownPanel.TranslateTo(0, -200, 300, Easing.CubicIn);
+    //        DropdownPanel.IsVisible = false;
+    //        isMenuVisible = false;
+    //    }
+    //    else
+    //    {
+    //        // نمایش منو
+    //        await DropdownPanel.TranslateTo(0, 0, 300, Easing.CubicOut);
+    //        DropdownPanel.IsVisible = true;
+    //        isMenuVisible = true;
+    //    }
+    //}
+
+
+
+
+    #region click
+
+    private async void OnBackClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("..");
+    }
+
+    private void OnMenuClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Menu", "Menu clicked", "OK");
+    }
+
+    // Tab Handlers
+    private async void OnSimpleTabClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("ساده", "حالت ساده انتخاب شد", "تایید");
+    }
+
+    private async void OnAdvancedTabClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("پیشرفته", "حالت پیشرفته + آنلاین انتخاب شد", "تایید");
+    }
+
+    // Top Action Handlers
+    private async void OnSettingsClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("تنظیمات", "تنظیمات سیستم", "تایید");
+    }
+
+    private async void OnAtGlanceClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("در یک نگاه", "نمای کلی سیستم", "تایید");
+    }
+
+    private async void OnCalendarClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("تقویم", "نمایش تقویم", "تایید");
+    }
+
+    private async void OnBookAppointmentClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("ثبت نوبت", "ثبت نوبت جدید", "تایید");
+    }
+
+    // Service Action Handlers
+    private async void OnCustomerFileClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//CustomerProfilePage");
+    }
+
+    private async void OnCancelClicked(object sender, EventArgs e)
+    {
+        bool result = await DisplayAlert("کنسل نوبت", "آیا مطمئن هستید که می‌خواهید این نوبت را کنسل کنید؟", "بله", "خیر");
+        if (result)
+        {
+            await DisplayAlert("کنسل شد", "نوبت با موفقیت کنسل شد", "تایید");
+        }
+    }
+
+    private async void OnSatisfactionClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("رضایت سنجی", "ارسال فرم رضایت سنجی", "تایید");
+    }
+
+    private async void OnEditAppointmentClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("ویرایش نوبت", "ویرایش اطلاعات نوبت", "تایید");
+    }
+
+    private async void OnContactCustomerClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("ارتباط با مشتری", "تماس با مشتری", "تایید");
+    }
+
+    // Bottom Navigation Handlers
+    private void OnBottomSettingsClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Settings", "Settings clicked", "OK");
+    }
+
+    private void OnListClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("List", "List clicked", "OK");
+    }
+
+    private void OnAddClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Add", "Add clicked", "OK");
+    }
+
+    private void OnSearchClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Search", "Search clicked", "OK");
+    }
+
+    private async void OnHomeClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//MainPage");
+    }
+
+
+    #endregion
 }
 
