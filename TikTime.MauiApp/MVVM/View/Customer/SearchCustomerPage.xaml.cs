1e3cb3a45f55a78caa6e550f3b33c726044f087e
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-20 18:58:36 +0330

    add page

--- TikTime.MauiApp/MVVM/View/Customer/SearchCustomerPage.xaml.cs
+++ TikTime.MauiApp/MVVM/View/Customer/SearchCustomerPage.xaml.cs
@@ -1,9 +1,35 @@
+using TikTime.MauiApp.MVVM.ViewModel.Customer;
+using TikTime.MauiApp.Service;
+
 namespace TikTime.MauiApp.MVVM.View.Customer;
 
 public partial class SearchCustomerPage : ContentPage
 {
-	public SearchCustomerPage()
-	{
-		InitializeComponent();
-	}
+	
+
+    private readonly ICustomerService _customerService;
+    public SearchCustomerPage(IServiceProvider serviceProvider)
+    {
+        InitializeComponent();
+        this._customerService = serviceProvider.GetRequiredService<ICustomerService>();
+        BindingContext = new ListCustomerViewModel(serviceProvider);
+
+
+    }
+
+    private void SearchBarNameCustomer_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        if (BindingContext is ListCustomerViewModel vm)
+        {
+            vm.SearchBarNameCustomer_OnTextChanged?.Execute(e.NewTextValue);
+        }
+    }
+
+    private void SearchBarNameCustomer_OnSearchButtonPressed(object? sender, EventArgs e)
+    {
+        if (BindingContext is ListCustomerViewModel vm)
+        {
+            vm.SearchBarNameCustomer_OnTextChanged?.Execute(SearchBarNameCustomer.Text);
+        }
+    }
 }
