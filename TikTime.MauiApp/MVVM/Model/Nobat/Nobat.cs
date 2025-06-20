45fa9dd27cabefac33d89d867af293561cfc24bb
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-20 18:58:12 +0330

    update by show items

--- TikTime.MauiApp/MVVM/Model/Nobat/Nobat.cs
+++ TikTime.MauiApp/MVVM/Model/Nobat/Nobat.cs
@@ -1,24 +1,40 @@
 ﻿using PropertyChanged;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-
+using PropertyChanged.fo;
 namespace TikTime.MauiApp.MVVM.Model.Nobat
 {
     [AddINotifyPropertyChangedInterface]
-    public class Nobat
+    public class Nobat: INotifyPropertyChanged
     {
+        private bool _showItems = false;
 
 
         public int Id { get; set; }
         public string Desc { get; set; }
         public string StartDate { get; set; }
         public string StartTime { get; set; }
-        public bool ShowItems { get; set; } = false;
 
+        public bool ShowItems
+        {
+            get { return _showItems; }
+            set
+            {
+                _showItems = value;
+                onPropertyChanged();
+            }
+        }
 
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void onPropertyChanged([CallerMemberName] string Probname = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(Probname));
+        }
         public int ServiceId { get; set; }
         public Service.Service Service { get; set; } = new();
 
