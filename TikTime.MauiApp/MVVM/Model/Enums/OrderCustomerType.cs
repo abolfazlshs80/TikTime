e0790c3f4b26b6cd995698194a65e647a3a29cb8
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-23 12:33:23 +0330

    add OrderCustomerType

---
+++ TikTime.MauiApp/MVVM/Model/Enums/OrderCustomerType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TikTime.MauiApp.MVVM.Model.Enums
+{
+    public enum OrderCustomerType
+    {
+        None,
+        Last,
+        Old
+    }
+}
