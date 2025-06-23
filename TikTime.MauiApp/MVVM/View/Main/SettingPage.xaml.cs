43b13806b905baaf04dfb481a3f0f2d24f54ba05
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-23 12:35:05 +0330

    update setting page

--- TikTime.MauiApp/MVVM/View/Main/SettingPage.xaml.cs
+++ TikTime.MauiApp/MVVM/View/Main/SettingPage.xaml.cs
@@ -1,3 +1,6 @@
+using System.Collections.ObjectModel;
+using TikTime.MauiApp.Service;
+
 namespace TikTime.MauiApp.MVVM.View.Main;
 
 public partial class SettingPage : ContentPage
@@ -5,5 +8,14 @@ public partial class SettingPage : ContentPage
 	public SettingPage()
 	{
 		InitializeComponent();
-	}
+        GroupsCustomer = new ObservableCollection<string>(FakeDataService.Instance.GroupsCustomer);
+        Ref = new ObservableCollection<string>(FakeDataService.Instance.Ref);
+        BindingContext = this;
+        PickerRef.ItemsSource = Ref;
+        PickerGroupCustomer.ItemsSource = GroupsCustomer;
+    }
+
+
+    public ObservableCollection<string> GroupsCustomer;
+    public ObservableCollection<string> Ref;
 }
