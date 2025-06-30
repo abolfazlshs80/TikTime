181713c9787a46b3123912e2e80176de84c3a5ff
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-30 21:05:11 +0330

    Add NottificationService and INottificationService for managing notifications

---
+++ TikTime.MauiApp/Service/NottificationService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TikTime.MauiApp.MVVM.Model.Notifications;
+
+namespace TikTime.MauiApp.Service
+{
+    public class NottificationService : INottificationService
+    {
+        public async Task<bool> Add(Nottification Nottification)
+        {
+            FakeDataService.Instance.Nottification.Add(Nottification);
+            return true;
+        }
+
+        public async Task<IEnumerable<Nottification>> GetAll()
+        {
+            return FakeDataService.Instance.Nottification;
+        }
+    }
+
+    public interface INottificationService
+    {
+        Task<bool> Add(Nottification Nottification);
+        Task<IEnumerable<Nottification>> GetAll();
+
+    }
+
+
+
+}
