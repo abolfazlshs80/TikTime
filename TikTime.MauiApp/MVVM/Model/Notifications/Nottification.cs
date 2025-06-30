3e54d125b7701c8af8c6020ec1ee6ddd184409d5
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-30 21:04:48 +0330

    Add Nottification model class for managing notifications

---
+++ TikTime.MauiApp/MVVM/Model/Notifications/Nottification.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TikTime.MauiApp.Tools.Static;
+using TikTime.MauiApp.Tools.Static.ExtentionMethod;
+namespace TikTime.MauiApp.MVVM.Model.Notifications
+{
+    public class Nottification
+    {
+        public Nottification()
+        {
+            
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+        public string TimeAgo { get { return Date.GetTimeAgo(); } }
+    }
+}
